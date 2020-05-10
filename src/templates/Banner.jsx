7f329711f4194c5fec7ea80346732a998bb0023b
Janumurthy <ljanani92@gmail.com>
2020-05-11 00:28:58 +0530

    Removing UIKit

--- src/templates/Banner.jsx
+++ src/templates/Banner.jsx
@@ -1,7 +1,5 @@
 import React, {Component} from "react";
 import Tribe from "../assets/images/Tribe.png";
-import '.././../node_modules/uikit/dist/css/uikit.css'
-import '.././../node_modules/uikit/dist/js/uikit.js'
 
 export default class Banner extends Component{ 
     constructor(props){
@@ -13,7 +11,7 @@ export default class Banner extends Component{
         return (
             <section id="coming-soon">
                 <div className ="container">
-                    <img src={Tribe} className ="symbol" alt="Tiki Taco" data-uk-scrollspy="{cls:'uk-animation-fade', delay:3000}"/>
+                    <img src={Tribe} className ="symbol" alt="Tiki Taco"/>
                 </div>
             </section>
         )
